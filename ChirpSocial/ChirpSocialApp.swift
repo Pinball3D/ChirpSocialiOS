cdb7c43be9431e26cdd6795d5f15d5c144f1e5d4
Andrew Smiley <smiley29kid@gmail.com>
2024-08-14 20:22:15 -0400

    Initial Commit

---
+++ ChirpSocial/ChirpSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChirpSocialApp.swift
+//  ChirpSocial
+//
+//  Created by Andrew Smiley on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ChirpSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChirpSocial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChirpSocial
+//
+//  Created by Andrew Smiley on 8/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
